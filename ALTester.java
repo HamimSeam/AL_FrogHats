68b45109b4ff094a351ae969f5df33edfeffdd44
jacob ng <jng20@stuy.edu>
2021-12-13 14:05:32 -0500

    skeleton

--- ALTester.java
+++ ALTester.java
@@ -17,18 +17,38 @@ public class ALTester {
 		return arrayString;
 	}
 
+	static boolean CheckOrdered(OrderedArrayList array) {
+		for ( int i = 0; i < array._info.size() - 1; i++ ) 
+			if ( array._info.get(i) > array._info.get(i + 1) ) 
+				return false;
+
+		return true;
+	}
+
+	static String PrintArray(OrderedArrayList array) {
+		String arrayString = "[";
+		for ( Integer num : array._info ) arrayString += num + " ,";
+		arrayString += "]";
+		return arrayString;
+	}
+
+
 	public static void main(String[] args){
 		ArrayList<Integer> Bob = new ArrayList<Integer>();
+		OrderedArrayList Dave = new OrderedArrayList();
 		//ArrayList<Integer> Charlie = new ArrayList<Integer>();
 		//Charlie.add(2);
 		//Charlie.add(2);
 
-		for ( int i = 0; i < 23; i++ )
+		for ( int i = 0; i < 23; i++ ) {
 			Bob.add( (int) (Math.random()*100) );
-
-		//System.out.println(PrintArray(Bob));
+			Dave.add( (int) (Math.random()*100));
+		}
+		System.out.println(PrintArray(Bob));
 		System.out.println(CheckOrdered(Bob));
+		System.out.println(CheckOrdered(Dave));
 		//System.out.println(CheckOrdered(Charlie));
-
+		System.out.println(PrintArray(Dave));
+		
 	}
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -2,33 +2,158 @@ import java.util.*;
 
 public class OrderedArrayList{
 
-    ArrayList<Integer> _info = new ArrayList<Integer>();  //got a deprecated api note when I forgot the <Integer> part
+    ArrayList<Integer> _data = new ArrayList<Integer>();  //got a deprecated api note when I forgot the <Integer> part
 
     public boolean add( int e ){
 
-        if(_info.size() == 0){
-            _info.add(e);
-            return true;
+        if(_data.size() == 0){
+            _data.add(newVal);
+            
         }else{
-            for(int i = 0; i < _info.size(); i++){
+            for(int i = 0; i < _data.size(); i++){
 
-              Integer indexV = _info.get(i);  //got a deprecated api note when I used new Integer
+              Integer indexV = _data.get(i);  //got a deprecated api note when I used new Integer
 
-              if(indexV.compareTo(e) == 0){
-                  _info.add(i+1, e);
-                  return true;
+              if(indexV.compareTo(newVal) == 0){
+                  _data.add(i+1, e);
+                  
               }
             }
             int j = 0;
-            while(_info.get(j).compareTo(e) < 0){
+            while(_data.get(j).compareTo(newVal) < 0){
                 j++;
-                if(j == _info.size()){
-                  _info.add(e);
-                  return true;
+                if(j == _data.size()){
+                  _data.add(newVal);
+                  
                 }
             }
-            _info.add(j,e);
-            return true;
+            _data.add(j,e);
+            
         }
     }
 }
+
+// Clyde "Thluffy" Sinclair
+// APCS pd00
+// Lab02
+// 2021-12-13
+
+/**********************************************
+ * class OrderedArrayList
+ * wrapper class for ArrayList
+ * maintains invariant that elements are ordered (ascending)
+ * (SKELETON)
+ **********************************************/
+
+
+import java.util.ArrayList;
+
+public class OrderedArrayList
+{
+
+  // instance of class ArrayList, holding objects of type Integer
+  // (i.e., objects of a class that implements interface Integer)
+  private ArrayList<Integer> _data;
+
+  // default constructor
+  // initializes instance variables
+  public OrderedArrayList()
+  {
+	ArrayList<Integer> _data = new ArrayList<Integer>();
+  }
+
+  public String toString()
+  {
+
+  }
+
+  public Integer remove( int i )
+  {
+	
+  }
+
+  public int size()
+  {
+
+  }
+
+  public Integer get( int i )
+  {
+
+  }
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a linear search to find appropriate index
+  public void addLinear(Integer newVal)
+  {
+	if(_data.size() == 0){
+            _data.add(newVal);
+            
+        }else{
+            for(int i = 0; i < _data.size(); i++){
+
+              Integer indexV = _data.get(i);  //got a deprecated api note when I used new Integer
+
+              if(indexV.compareTo(newVal) == 0){
+                  _data.add(i+1, newVal);
+                  
+              }
+            }
+            int j = 0;
+            while(_data.get(j).compareTo(newVal) < 0){
+                j++;
+                if(j == _data.size()){
+                  _data.add(newVal);
+                  
+                }
+            }
+            _data.add(j, newVal);
+            
+  }
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a binary search to find appropriate index
+  public void addBinary(Integer newVal)
+  {
+	if (_data.size() == 0)
+		_data.add(newVal);
+	else {
+		for (int i = 0; i < _data.size(); i++){
+
+              Integer indexV = _data.get(i);  //got a deprecated api note when I used new Integer
+
+              if(indexV.compareTo(newVal) == 0){
+                  _data.add(i+1, newVal);
+              }
+              
+        int middle = _data.size()/2;
+        if ( newVal > _data.get(middle) ) {
+        	int j = _data.size() - middle;
+        	while ( _data.get(j).compareTo(newVal) < 0 ) {
+        	if ( j == _data.size() )
+        		_data.add(newVal)
+        		
+            }
+  }	
+
+  // main method solely for testing purposes
+  public static void main( String[] args )
+  {
+    /*-----v-------move-me-down-----------------v--------
+    OrderedArrayList Franz = new OrderedArrayList();
+    // testing linear search
+    for( int i = 0; i < 15; i++ )
+      Franz.addLinear( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+    // testing binary search
+    Franz = new OrderedArrayList();
+    for( int i = 0; i < 15; i++ ) 
+      Franz.addBinary( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+      =====^====================================^=========*/
+
+  }//end main()
+
+}//end class OrderedArrayList
